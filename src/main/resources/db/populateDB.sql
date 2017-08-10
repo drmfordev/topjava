0f86d3fd0fd68f5d9f4822c7ba6924a3080775d2
Andrii Holub <andr.holub@gmail.com>
2017-08-10 16:10:30 +0300

    pre-commit

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -11,3 +12,22 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Завтрак', 1000, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Обед', 500, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Ужин', 1500, 100000);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Завтрак', 1000, 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Обед', 500, 100001);
+
+INSERT INTO meals (description, calories, user_id)
+VALUES ('Ужин', 1500, 100001);
+
