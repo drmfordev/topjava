1ba84024a602e9524761e2758435f516a9476259
Andrii Holub <andr.holub@gmail.com>
2017-08-30 22:21:42 +0300

    HW04 optional done

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -11,10 +17,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 
+import static org.slf4j.LoggerFactory.getLogger;
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
@@ -30,6 +39,34 @@ public class MealServiceTest {
     static {
         SLF4JBridgeHandler.install();
     }
+    private static final Logger log = getLogger(MealServiceTest.class);
+    private static String watchedLog;
+
+    @AfterClass
+    public static void printLog(){
+        log.warn(watchedLog);
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+
+        LocalTime time;
+
+
+        @Override
+        protected void starting(Description description) {
+            time = LocalTime.now();
+        }
+
+        @Override
+        protected void finished(Description description) {
+             long executionTime = Duration.between(time, LocalTime.now()).toMillis();
+            log.warn("execution time - " + executionTime + " ms");
+            watchedLog += (description.getMethodName().concat(" execution time - ".concat(String.valueOf(executionTime)).concat(" ms\n")));
+        }
+    };
 
     @Autowired
     private MealService service;
@@ -40,8 +77,9 @@ public class MealServiceTest {
         MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), service.getAll(USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testDeleteNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
         service.delete(MEAL1_ID, 1);
     }
 
@@ -58,8 +96,9 @@ public class MealServiceTest {
         MATCHER.assertEquals(ADMIN_MEAL1, actual);
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testGetNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
         service.get(MEAL1_ID, ADMIN_ID);
     }
 
@@ -70,8 +109,9 @@ public class MealServiceTest {
         MATCHER.assertEquals(updated, service.get(MEAL1_ID, USER_ID));
     }
 
-    @Test(expected = NotFoundException.class)
+    @Test
     public void testUpdateNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
         service.update(MEAL1, ADMIN_ID);
     }
 
