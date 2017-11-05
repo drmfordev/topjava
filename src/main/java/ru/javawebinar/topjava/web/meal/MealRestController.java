ba51b777fada226c1d5ccfc7efdf75483562d482
Andrii Holub <andr.holub@gmail.com>
2017-11-05 21:19:45 +0200

    added RestMealController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -3,7 +3,10 @@ package ru.javawebinar.topjava.web.meal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.AuthorizedUser;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
@@ -11,10 +14,15 @@ import ru.javawebinar.topjava.to.MealWithExceed;
 import ru.javawebinar.topjava.util.DateTimeUtil;
 import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Objects;
 
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
@@ -29,37 +37,62 @@ public class MealRestController {
         this.service = service;
     }
 
-    public Meal get(int id) {
+    @GetMapping("/meals/update/{mealId}")
+    public String update(@PathVariable int mealId, Model model) {
         int userId = AuthorizedUser.id();
-        log.info("get meal {} for userId={}", id, userId);
-        return service.get(id, userId);
+        log.info("get meal {} for userId={}", mealId, userId);
+        model.addAttribute("meal",service.get(mealId, userId));
+        return "mealForm";
     }
-
-    public void delete(int id) {
+    @GetMapping("/meals/delete/{mealId}")
+    public String delete(@PathVariable int mealId) {
         int userId = AuthorizedUser.id();
-        log.info("delete meal {} for userId={}", id, userId);
-        service.delete(id, userId);
-    }
+        log.info("delete meal {} for userId={}", mealId, userId);
+        service.delete(mealId, userId);
 
-    public List<MealWithExceed> getAll() {
+        return "redirect:/meals";
+    }
+    @GetMapping("/meals")
+    public String getAll(Model model) {
         int userId = AuthorizedUser.id();
         log.info("getAll for userId={}", userId);
-        return MealsUtil.getWithExceeded(service.getAll(userId), AuthorizedUser.getCaloriesPerDay());
+        model.addAttribute("meals",MealsUtil.getWithExceeded(service.getAll(userId), AuthorizedUser.getCaloriesPerDay()));
+        return "meals";
     }
 
-    public Meal create(Meal meal) {
+    @GetMapping("/meals/create")
+    public String createMeal(Model model) {
         int userId = AuthorizedUser.id();
-        log.info("create {} for userId={}", meal, userId);
-        checkNew(meal);
-        return service.create(meal, userId);
+        log.info("create meal for userId={}", userId);
+        model.addAttribute("message","Create meal");
+        return "mealForm";
     }
 
-    public void update(Meal meal, int id) {
+    @PostMapping("/meals")
+    public String createOrUpdate(HttpServletRequest request) {
         int userId = AuthorizedUser.id();
-        log.info("update {} with id={} for userId={}", meal, id, userId);
-        assureIdConsistent(meal, id);
-        service.update(meal, userId);
+        log.info("create {} for userId={}", request, userId);
+        Meal meal = new Meal(
+                    LocalDateTime.parse(request.getParameter("dateTime")),
+                    request.getParameter("description"),
+                    Integer.parseInt(request.getParameter("calories")));
+        System.out.println(request.getParameter("id"));
+            if (request.getParameter("id").isEmpty()) {
+                service.create(meal, userId);
+            } else {
+
+                service.update(meal, userId);
+            }
+
+        return "redirect:/meals";
     }
+
+//    public void update(Meal meal, int id) {
+//        int userId = AuthorizedUser.id();
+//        log.info("update {} with id={} for userId={}", meal, id, userId);
+//        assureIdConsistent(meal, id);
+//        service.update(meal, userId);
+//    }
 
     /**
      * <ol>Filter separately
@@ -67,17 +100,26 @@ public class MealRestController {
      *   <li>by time for every date</li>
      * </ol>
      */
-    public List<MealWithExceed> getBetween(LocalDate startDate, LocalTime startTime, LocalDate endDate, LocalTime endTime) {
+    @PostMapping("/filter")
+    public String getBetween(HttpServletRequest request, Model model) {
         int userId = AuthorizedUser.id();
+            LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+            LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+            LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+            LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+
         log.info("getBetween dates({} - {}) time({} - {}) for userId={}", startDate, endDate, startTime, endTime, userId);
 
-        return MealsUtil.getFilteredWithExceeded(
+        model.addAttribute("meals", MealsUtil.getFilteredWithExceeded(
                 service.getBetweenDates(
                         startDate != null ? startDate : DateTimeUtil.MIN_DATE,
                         endDate != null ? endDate : DateTimeUtil.MAX_DATE, userId),
                 startTime != null ? startTime : LocalTime.MIN,
                 endTime != null ? endTime : LocalTime.MAX,
                 AuthorizedUser.getCaloriesPerDay()
-        );
+        ));
+        return "meals";
     }
+
+
 }
